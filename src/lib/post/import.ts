6769a8ba72c3f41a02fb0fdab0821d3473ef3e05
Ken Mueller <kenmueller0@gmail.com>
2023-05-19 16:29:29 -0700

    turned on csr

--- src/lib/post/import.ts
+++ src/lib/post/import.ts
@@ -1,7 +1,11 @@
+import type { SvelteComponentTyped } from 'svelte'
+
 export default interface PostImport {
 	metadata: {
 		title: string
 		date: string
 	}
-	default: unknown
+	default: {
+		new (...args: unknown[]): SvelteComponentTyped
+	}
 }
